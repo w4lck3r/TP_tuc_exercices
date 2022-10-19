fe84ffa83f68386b4a5af503c01c5382444dcee0
yann <yann.coornart@ext.leroymerlin.fr>
2022-10-19 20:34:24 +0200

    feat(): Init exercices

---
+++ main.py
@@ -0,0 +1,23 @@
+import math
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def home():
+    return "Hello World"
+
+
+@app.get("/{number}")
+def return_square(number: int):
+    return get_square(number)
+
+
+@app.get("/{number}")
+def return_square_twice(number: int):
+    return get_square(number) + get_square(number)
+
+
+def get_square(number: int):
+    return math.pow(number, 2)
